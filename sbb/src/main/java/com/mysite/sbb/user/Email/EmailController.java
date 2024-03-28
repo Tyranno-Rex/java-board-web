03e846dbf7048d26ccf04a5bf476ca46be0bfc76
eunseong <jsilvercastle@gmail.com>
2024-03-28 15:44:07 +0900

    hello

--- sbb/src/main/java/com/mysite/sbb/user/Email/EmailController.java
+++ sbb/src/main/java/com/mysite/sbb/user/Email/EmailController.java
@@ -23,6 +23,7 @@ public class EmailController {
 
     public static void sendEmail(String username, String email, String newPassword) {
         try {
+//            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\admin\\project\\back-up\\sbb\\src\\main\\jaC:\\Users\\admin\\project\\back-up\\sbb\\src\\main\\java\\com\\mysite\\sbb\\user\\Email\\Email-password.txt", Charset.forName("UTF-8")));
             BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\admin\\project\\back-up\\sbb\\src\\main\\jaC:\\Users\\admin\\project\\back-up\\sbb\\src\\main\\java\\com\\mysite\\sbb\\user\\Email\\Email-password.txt", Charset.forName("UTF-8")));
             String firstLine = reader.readLine();
             password = firstLine;
